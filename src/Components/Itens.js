03235a1fc208bac38ede7134735d2093360359d6
Daniel Franchi <daniel2mf@yahoo.com.br>
2021-01-20 23:19:30 -0300

    projeto react-props-components

---
+++ src/Components/Itens.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Itens = ({itens}) => {
+    return (
+        <ul>
+            <li>{itens}</li>
+        </ul>
+    )
+}
+
+export default Itens
---
+++ src/Components/ListaCompras.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Itens from './Itens'
+
+const ListaCompras = () => {
+    return (
+        <>
+            <Itens itens='Pizza' />
+            <Itens itens='MacDonalds' />
+            <Itens itens='Sorvete' />
+            <Itens itens='Bolo' />
+            <Itens itens='Coca-Cola' />
+        </>
+    )
+}
+
+export default ListaCompras
